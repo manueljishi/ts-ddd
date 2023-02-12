37241ea37eba70810f5a579cd948bd8bdf3a749e
manueljishi <manueljishi@gmail.com>
2023-02-12 23:19:38 +0100

    [feature] courses mock repository and schema validation

--- tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
+++ tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
@@ -2,17 +2,16 @@ import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domai
 import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
 
 export class CourseRepositoryMock implements CourseRepository {
-  private mockSave = jest.fn();
+  private saveMock = jest.fn();
 
   async save(course: Course): Promise<void> {
-    this.mockSave(course);
+    this.saveMock(course);
   }
 
   assertLastSavedCourseIs(expected: Course): void {
-    const mock = this.mockSave.mock;
+    const mock = this.saveMock.mock;
     const lastSavedCourse = mock.calls[mock.calls.length - 1][0] as Course;
     expect(lastSavedCourse).toBeInstanceOf(Course);
     expect(lastSavedCourse.id).toEqual(expected.id);
   }
-
 }
--- tests/apps/mooc/backend/features/courses/create-course.feature
+++ tests/apps/mooc/backend/features/courses/create-course.feature
@@ -7,9 +7,21 @@ Feature: Create a new course
     Given I send a PUT request to "/courses/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
     """
     {
+      "id":"ef8ac118-8d7f-49cc-abec-78e0d05af80a",
       "name": "The best course",
       "duration": "5 hours"
     }
     """
     Then the response status code should be 201
     And the response should be empty
+
+  Scenario: An invalid non existing course
+    Given I send a PUT request to "/courses/8d7f9cc-abec-78e0d05af80a" with body:
+    """
+    {
+      "id":"8d7f9cc-abec-78e0d05af80a",
+      "name": 5,
+      "duration": "5 hours"
+    }
+    """
+    Then the response status code should be 422
